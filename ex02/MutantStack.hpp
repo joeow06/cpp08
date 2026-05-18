0e41b76c0d3232bc2d68a4b477cb3aa00cf547a5
joeow06 <joeow06@gmail.com>
2026-05-18 13:03:21 +0800

    feat: added MutantStack implementation

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/17 15:00:54 by jow               #+#    #+#             */
+/*   Updated: 2026/05/17 15:00:55 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+# include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+	public:
+		MutantStack() {}
+		MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+		MutantStack& operator=(const MutantStack &other)
+		{
+			std::stack<T>::operator=(other);
+			return (*this);
+		}
+		~MutantStack() {}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		iterator begin()
+		{
+			return (this->c.begin());
+		}
+		iterator end()
+		{
+			return (this->c.end());
+		}
+		const_iterator begin() const
+		{
+			return (this->c.begin());
+		}
+		const_iterator end() const
+		{
+			return (this->c.end());
+		}
+} ;
+
+#endif
